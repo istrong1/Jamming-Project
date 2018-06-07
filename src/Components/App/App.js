840b08f91bc5d9303cd1d4fdcd10c668021a98c8
istrong1 <38330822+istrong1@users.noreply.github.com>
2018-06-07 14:52:01 -0500

    Update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,30 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
+import Playlist from  '../Playlist/Playlist';
+import Spotify from '/Users/ianstrong/ReactApps/jamming/src/util/Spotify.js'
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      SearchResults:[],
+      playlistName: 'Sample playlist',
+      playlistTracks: []
+    }
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
+  }
+
+  addTrack(track) {
+    let currentPlaylistArray = this.state.playlistTracks.concat(track);
+    this.setState({ playlistTracks: currentPlaylistArray });
+  }
+
+  removeTrack(track) {
+    let currentPlaylistArray = this.state.playlistTracks;
+    this.setState({ playlistTracks: currentPlaylistArray.filter(checkTrack => checkTrack.id !== track.id) });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name});
+  }
+
+  savePlaylist() {
+    const playlistArray = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savedPlayList(this.state.playlistName, playlistArray).then(() => {
+    this.setState({ playlistTracks: [], playlistName: 'New Playlist'});
+    });
+  }
+
+  search(term) {
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    });
+  }
+
   render() {
     return (
       <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <!-- Add a SearchBar component -->
-    <div className="App-playlist">
-      <!-- Add a SearchResults component -->
-      <!-- Add a Playlist component -->
-    </div>
-  </div>
-</div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+          <div className="App">
+            <SearchBar onSearch={this.search} />
+            <div className="App-playlist">
+              <SearchResults
+                searchResults={this.state.searchResults}
+                onAdd={this.addTrack}
+                onRemove={this.removeTrack} />
+                <playlist
+                  name={this.state.playlistName}
+                  tracks={this.state.playlistTracks}
+                  onRemove={this.removeTrack}
+                  onNameChange={this.updatePlaylistName}
+                  onSave={this.savePlaylist}
+                  />
+              </div>
+          </div>
       </div>
     );
   }
