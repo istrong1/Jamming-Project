0a5b51ee41a20a786cca8da239ef90775a81d658
istrong1 <38330822+istrong1@users.noreply.github.com>
2018-06-07 14:53:24 -0500

    Update TrackList.js

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import Track from '../Track/Track';
 import './TrackList.css';
+import Track from '../track/Track';
 
-class TrackList extends Component {
+class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-      <!-- You will add a map method that renders a set of Track components  -->
-      </div>
-    )
-    }
+        {this.props.tracks.map(track => <Track track={track} key={track.id}/>)}
+        </div>
+      );
   }
-
+}
 export default TrackList;
