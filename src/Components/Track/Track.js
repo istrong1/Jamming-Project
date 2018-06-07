741dfd41ee01fa2bcaae0bfb2f460604b66fffbf
istrong1 <38330822+istrong1@users.noreply.github.com>
2018-06-07 14:52:49 -0500

    Update Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,19 +1,37 @@
 import React from 'react';
-import
-import
+import './Track.css';
 
-class Track extends Component {
+class Track extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (this.props.action==='+') {
+      this.props.addTrack(this.props.track, this.props.id);
+    } else {
+      this.props.removeTrack(this.props.id);
+    }
+  }
+
+  setAddedClass() {
+    return this.props.track.added && this.props.action === '+' ? 'Added' : '';
+  }
+
   render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3><!-- track name will go here --></h3>
-          <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+          <h3
+            className={this.setAddedClass()}>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
-        <a className="Track-action"><!-- + or - will go here --></a>
+        <a
+          onClick={this.handleClick}
+          className={"Track-action " + this.setAddedClass()}>{this.props.action}</a>
       </div>
-    )
-    }
+    );
   }
-
+}
 export default Track;
